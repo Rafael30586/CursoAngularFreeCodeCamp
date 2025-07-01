2153575d79981bd195008449eede8f1a7fdc8081
Rafael305 <fraq86@gmail.com>
2025-07-01 19:08:32 -0300

    ngSwitch

--- hotelinventoryapp2/src/app/app.ts
+++ hotelinventoryapp2/src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Rooms } from './rooms/rooms';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'hinv-root', // el que viene por defecto es app-root, le he cambiado el nombre en el archivo angular.json y y en el index.html
-  imports: [RouterOutlet,Rooms],
+  imports: [RouterOutlet,Rooms, CommonModule],
   templateUrl: './app.html',
   /*template: `<h1>Hello world from inline template</h1>
    <p>Angular is awesome</p>`,*/
@@ -13,4 +14,6 @@ import { Rooms } from './rooms/rooms';
 })
 export class App {
   protected title = 'hotelinventoryapp2';
+
+  role = 'Admin';
 }
